8469bbd8a020e00cec2a4741ee3bbde5715f3f60
Apoorv Nigam <apoorvnigam3302@gmail.com>
2021-07-10 13:12:57 +0530

    Payment Processign emulator problem

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -9,6 +9,12 @@ from "react-router-dom";
 import Login from './Login';
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  'pk_test_51JBN4pSF9tR2fOGq27rrLd1NO53cCttkz8lYM70k3mUUR44TOWMVMvULEG7EGVSP5qHcQdpzu9x9CKC48UN9cZew00t2HFoNVD'
+  );
 
 function App() {
 
@@ -52,7 +58,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+               <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
